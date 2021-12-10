53ebd3aba8ab609ea47496a0647e5f10b64a8c0e
Lolpawer <dadu.petrosino@gmail.com>
2021-12-10 15:45:28 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 //programma che stampa in console i numeri da 1 a 100
+//modifica : multipli di 3 stampa fizz
+//multipli di 5 stampa buzz
+//multipli di 3 e 5 stampa fizzbuzz
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    }
+    else if (i % 3 === 0) {
+        console.log('Fizz');
+    }
+    else if (i % 5 === 0) {
+        console.log('Buzz');
+    }
+    else {
+        console.log(i);
+    }
 };
